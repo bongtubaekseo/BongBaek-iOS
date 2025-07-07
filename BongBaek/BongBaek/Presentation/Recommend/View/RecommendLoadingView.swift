4f8d8738013d5cfa7ffb9cb1dc9eed679de97a9c
hyunw-kang <vpffp368@naver.com>
2025-07-08 01:11:08 +0900

    [feat] RecommendLoading UI 작업

---
+++ BongBaek/BongBaek/Presentation/Recommend/View/RecommendLoadingView.swift
@@ -0,0 +1,44 @@
+//
+//  RecommendLoadingView.swift
+//  BongBaek
+//
+//  Created by hyunwoo on 7/8/25.
+//
+import SwiftUI
+
+struct RecommendLoadingView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.gray900
+                    .ignoresSafeArea(.all)
+                
+                VStack(spacing: 0) {
+                    CustomNavigationBar(title: "금액추천중")
+                    VStack(spacing: 30) {
+                        Spacer()
+                        LottieTest(animationFileName: "find_amount", loopMode: .loop)
+                            .frame(width: 151, height: 140)
+                        
+                        Text("{username}님을 위한\n급액을 찾고 있어요")
+                            .titleSemiBold18()
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(.gray100)
+                                        
+                        Text("잠시만 기다려주세요")
+                            .bodyRegular14()
+                            .foregroundColor(.gray400)
+                        
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+            }
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+#Preview {
+    RecommendLoadingView()
+}
