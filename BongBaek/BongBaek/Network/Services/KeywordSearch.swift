74d3a0fc780b144fd0c44cb3cab832f3b4f19fe3
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-07-09 12:18:18 +0900

    [feat] 컴색어 전송 및 결과 반환 가능한 KeyWordSearch 객체 생성

---
+++ BongBaek/BongBaek/Network/Services/KeywordSearch.swift
@@ -0,0 +1,55 @@
+//
+//  KeywordSearch.swift
+//  BongBaek
+//
+//  Created by 임재현 on 7/9/25.
+//
+
+import SwiftUI
+import Alamofire
+
+
+/// TextField 에 장소 검색시 해당 String 값을 쿼리로 parameter 에 담아서 전송후, 카카오에서 제공하는 해당 검색어 기반 위치 정보를 받아오는 객체
+class KeyWordSearch: ObservableObject {
+    @Published var searchResults: [KLDocument] = []
+    
+    @Published var query = "" {
+        didSet {
+            print("set")
+            search()
+        }
+    }
+    
+    func search() {
+        let url = "https://dapi.kakao.com/v2/local/search/keyword.json"
+        let headers: HTTPHeaders = [
+            "Authorization": "KakaoAK API KEY "
+        ]
+        let parameters: [String: Any] = [
+            "query": query,
+            "x": 126.97806,
+            "y": 37.56667,
+            "sort": "distance"
+        ]
+        
+        AF.request(url, method: .get, parameters: parameters, headers: headers)
+            .responseDecodable(of: KakaoSearchResponse.self) { response in
+                switch response.result {
+                case .success(let kakaoResponse):
+                    print("성공: \(kakaoResponse)")
+                    
+                    // 검색 결과를 배열에 추가
+                    self.searchResults.append(contentsOf: kakaoResponse.documents)
+                    
+                case .failure(let error):
+                    print("에러: \(error.localizedDescription)")
+                    
+                    // 디버깅을 위한 상세 에러 정보
+                    if let data = response.data {
+                        print("응답 데이터: \(String(data: data, encoding: .utf8) ?? "없음")")
+                    }
+                }
+            }
+    }
+ 
+}
