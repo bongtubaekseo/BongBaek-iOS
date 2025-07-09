699912478d78c78bdeaff04f8a3c2360f84c2125
hyunw-kang <vpffp368@naver.com>
2025-07-09 14:49:35 +0900

    [feat] RecommendCostView 작업

--- BongBaek/BongBaek/Presentation/Recommend/View/RecommendLottie.swift
+++ BongBaek/BongBaek/Presentation/Recommend/View/RecommendLottie.swift
@@ -25,81 +25,8 @@ struct RecommendLottie: View {
             }
             
             if showRectangle {
-                VStack(spacing: 20) {
-                    Text("결혼식")
-                        .bodyMedium14()
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 16)
-                        .padding(.vertical, 8)
-                        .background(
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(.primaryNormal, lineWidth: 1)
-                                .background(.gray750)
-                        )
-                    
-                    VStack(spacing: 20) {
-                        Text("추천 금액")
-                            .captionRegular12()
-                            .foregroundColor(.white)
-                            .padding(.horizontal, 8)
-                            .padding(.vertical, 2)
-                            .background(
-                                RoundedRectangle(cornerRadius: 4)
-                                    .fill(.primaryNormal)
-                            )
-                        HStack(alignment: .bottom, spacing: 4) {
-                            Text("100,000")
-                                .font(.system(size: 46, weight: .bold))
-                                .foregroundStyle(
-                                    LinearGradient(
-                                        colors: [
-                                            Color(hex: "#4E62FF"),
-                                            Color(hex: "#502EFF")
-                                        ],
-                                        startPoint: .leading,
-                                        endPoint: .trailing
-                                    )
-                                )
-                            
-                            Text("원")
-                                .titleSemiBold22()
-                                .foregroundColor(.gray600)
-                                .padding(.bottom, 8)
-                        }
-                        VStack(spacing: 2) {
-                            Text("적절한 금액이에요!")
-                                .bodyMedium16()
-                                .foregroundColor(.white)
-                            
-                            Text("알려주신 정보를 고려한 추천입니다")
-                                .bodyRegular14()
-                                .foregroundColor(.gray200)
-                        }
-                        .padding(.horizontal, 24)
-                        .padding(.vertical, 10)
-                        .background(
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(.gray750.opacity(0.6))
-                        )
-                    }
-                    .padding(.horizontal, 30)
-                    .padding(.vertical, 30)
-                    .background(
-                        RoundedRectangle(cornerRadius: 12)
-                            .fill(
-                                LinearGradient(
-                                    colors: [
-                                        Color(hex: "#A6BEF3"),
-                                        Color(hex: "#D3D9FF")
-                                    ],
-                                    startPoint: .topLeading,
-                                    endPoint: .bottomTrailing
-                                )
-                            )
-                    )
-                    .padding(.horizontal, 20)
-                }
-                .transition(.opacity.combined(with: .scale))
+                RecommendCostView()
+                    .transition(.opacity.combined(with: .scale))
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
