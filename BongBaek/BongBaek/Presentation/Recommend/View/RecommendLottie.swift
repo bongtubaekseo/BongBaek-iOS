bfec2d95cc7e748ba0ecde97df9b9d0a57eb339e
hyunw-kang <vpffp368@naver.com>
2025-07-13 21:58:55 +0900

    [feat] LottieView에 AmountRangeSection추가

--- BongBaek/BongBaek/Presentation/Recommend/View/RecommendLottie.swift
+++ BongBaek/BongBaek/Presentation/Recommend/View/RecommendLottie.swift
@@ -7,7 +7,7 @@
 import SwiftUI
 
 struct RecommendLottie: View {
-    @StateObject private var viewModel = RecommendationAmountViewModel()
+    @StateObject private var lottieviewModel = RecommendCostViewModel()
     @State private var showRectangle = false
 
     @EnvironmentObject var router: NavigationRouter
@@ -28,9 +28,8 @@ struct RecommendLottie: View {
                         }
                     )
                     
-                    // 삽입된 코드 부분
                     VStack(spacing: 20) {
-                        //amountRangeSection
+                        amountRangeSection
                         categorySection
                     }
                     .padding(.horizontal, 20)
@@ -52,123 +51,115 @@ struct RecommendLottie: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.gray900)
     }
-}
-
-//var amountRangeSection: some View {
-//    VStack(alignment: .leading, spacing: 16) {
-//        Text("적정 범위")
-//            .titleSemiBold18()
-//            .foregroundColor(.white)
-//        
-//        //MARK: - 커스텀 슬라이더, (필요 없으면 삭제해도 됨.)
-//        VStack(spacing: 8) {
-//            ZStack(alignment: .leading) {
-//                RoundedRectangle(cornerRadius: 4)
-//                    .fill(Color(hex:"#292929"))
-//                    .frame(width: 300, height: 12)
-//                
-//                RoundedRectangle(cornerRadius: 4)
-//                    .fill(
-//                           LinearGradient(
-//                               colors: [
-//                                   Color(hex: "#502EFF"),
-//                                   Color(hex: "#807FFF")
-//                               ],
-//                               startPoint: .leading,
-//                               endPoint: .trailing
-//                           )
-//                       )
-//                    .frame(width: CGFloat(viewModel.sliderProgress) * 300, height: 12)
-//            }
-//            .frame(maxWidth: .infinity, alignment: .leading)
-//            .gesture(
-//                DragGesture()
-//                    .onChanged { value in
-//                        let progress = min(max(0, value.location.x / 300), 1)
-//                        let newAmount = Int(progress * Double(viewModel.maxAmount - viewModel.minAmount)) + viewModel.minAmount
-//                        viewModel.updateSliderAmount(to: Double(newAmount))
-//                    }
-//            )
-//            
-//            HStack {
-//                Text("\(viewModel.formattedMinAmount)원")
-//                    .captionRegular12()
-//                    .foregroundColor(.gray400)
-//                
-//                Spacer()
-//                
-//                Text("\(viewModel.formattedMaxAmount)원")
-//                    .captionRegular12()
-//                    .foregroundColor(.gray400)
-//            }
-//        }
-//    }
-//    .padding(16)
-//    .background(
-//        RoundedRectangle(cornerRadius: 10)
-//            .fill(Color.black)
-//    )
-//}
-
-// MARK: - Category Section
-var categorySection: some View {
-    HStack(spacing: 12) {
-        HStack(spacing: 12) {
-            ZStack {
-                Circle()
-                    .fill(.gray900)
-                    .frame(width: 40, height: 40)
-                
-                Image("icon_star")
-            }
-            VStack(alignment: .leading, spacing: 2) {
-                Text("경조사 종류")
-                    .captionRegular12()
-                    .foregroundColor(.gray400)
-                Text("결혼식")
-                    .bodyMedium16()
-                    .foregroundColor(.white)
-            }
+    
+    // MARK: - Amount Range Section
+    var amountRangeSection: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("적정 범위")
+                .titleSemiBold18()
+                .foregroundColor(.white)
             
-            Spacer()
-        }
-        .padding(.horizontal, 14)
-        .padding(.vertical, 14)
-        .frame(width: 163, height: 70)
-        .background(
-            RoundedRectangle(cornerRadius: 12)
-                .fill(Color("gray750"))
-        )
-        
-        HStack(spacing: 12) {
-            ZStack {
-                Circle()
-                    .fill(.gray900)
-                    .frame(width: 40, height: 40)
+            //MARK: - 커스텀 슬라이더, (필요 없으면 삭제해도 됨.)
+            VStack(spacing: 8) {
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(Color(hex:"#292929"))
+                        .frame(width: 300, height: 12)
+                    
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(
+                               LinearGradient(
+                                   colors: [
+                                       Color(hex: "#502EFF"),
+                                       Color(hex: "#807FFF")
+                                   ],
+                                   startPoint: .leading,
+                                   endPoint: .trailing
+                               )
+                           )
+                        .frame(width: 0, height: 12)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
                 
-                Image("icon_location 2")
+                HStack {
+                    Text("\(lottieviewModel.formattedMinAmount)원")
+                        .captionRegular12()
+                        .foregroundColor(.gray400)
+                    
+                    Spacer()
+                    
+                    Text("\(lottieviewModel.formattedMaxAmount)원")
+                        .captionRegular12()
+                        .foregroundColor(.gray400)
+                }
             }
-            VStack(alignment: .leading, spacing: 2) {
-                Text("장소")
-                    .captionRegular12()
-                    .foregroundColor(.gray400)
-                Text("강남 웨딩홀")
-                    .bodyMedium16()
-                    .foregroundColor(.white)
-            }
-            
-            Spacer()
         }
-        .padding(.horizontal, 14)
-        .padding(.vertical, 14)
-        .frame(width: 164, height: 70)
+        .padding(16)
         .background(
-            RoundedRectangle(cornerRadius: 12)
-                .fill(Color("gray750"))
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.black)
         )
     }
-}
 
+    // MARK: - Category Section
+    var categorySection: some View {
+        HStack(spacing: 12) {
+            HStack(spacing: 12) {
+                ZStack {
+                    Circle()
+                        .fill(.gray900)
+                        .frame(width: 40, height: 40)
+                    
+                    Image("icon_star")
+                }
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("경조사 종류")
+                        .captionRegular12()
+                        .foregroundColor(.gray400)
+                    Text("결혼식")
+                        .bodyMedium16()
+                        .foregroundColor(.white)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal, 14)
+            .padding(.vertical, 14)
+            .frame(width: 163, height: 70)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color("gray750"))
+            )
+            
+            HStack(spacing: 12) {
+                ZStack {
+                    Circle()
+                        .fill(.gray900)
+                        .frame(width: 40, height: 40)
+                    
+                    Image("icon_location 2")
+                }
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("장소")
+                        .captionRegular12()
+                        .foregroundColor(.gray400)
+                    Text("강남 웨딩홀")
+                        .bodyMedium16()
+                        .foregroundColor(.white)
+                }
+                
+                Spacer()
+            }
+            .padding(.horizontal, 14)
+            .padding(.vertical, 14)
+            .frame(width: 164, height: 70)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color("gray750"))
+            )
+        }
+    }
+}
 //#Preview {
 //    RecommendLottie()
 //}
