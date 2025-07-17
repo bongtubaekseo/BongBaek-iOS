42eaafb5945e71a04710c3b60fb4b98ef4341399
hyunw-kang <vpffp368@naver.com>
2025-07-17 17:37:46 +0900

    [fix] 상세정보 Icon_Image 변경

--- BongBaek/BongBaek/Presentation/Record/View/AllRecordsView.swift
+++ BongBaek/BongBaek/Presentation/Record/View/AllRecordsView.swift
@@ -260,10 +260,10 @@ struct AllRecordsView: View {
         VStack(alignment: .leading, spacing: 24) {
             DetailRow(image: "icon_person_16", title: "이름", value: eventDetail.hostInfo.hostName)
             DetailRow(image: "icon_nickname_16", title: "별명", value: eventDetail.hostInfo.hostNickname)
-            DetailRow(image: "icon_event_16", title: "관계", value: eventDetail.eventInfo.relationship, valueTextColor: .primaryNormal, valueBackgroundColor: .primaryBg)
+            DetailRow(image: "icon_relation", title: "관계", value: eventDetail.eventInfo.relationship, valueTextColor: .primaryNormal, valueBackgroundColor: .primaryBg)
             DetailRow(image: "icon_event_16", title: "경조사", value: eventDetail.eventInfo.eventCategory, valueTextColor: .primaryNormal, valueBackgroundColor: .primaryBg)
-            DetailRow(image: "icon_event_16", title: "경조사비", value: "\(eventDetail.eventInfo.cost.formatted())원")
-            DetailRow(image: "icon_event_16", title: "참석여부", value: eventDetail.eventInfo.isAttend ? "참석" : "불참", valueTextColor: .primaryNormal, valueBackgroundColor: .primaryBg)
+            DetailRow(image: "icon_coin_16", title: "경조사비", value: "\(eventDetail.eventInfo.cost.formatted())원")
+            DetailRow(image: "icon_check 1", title: "참석여부", value: eventDetail.eventInfo.isAttend ? "참석" : "불참", valueTextColor: .primaryNormal, valueBackgroundColor: .primaryBg)
             DetailRow(image: "icon_event_16", title: "날짜", value: eventDetail.eventInfo.eventDate, valueTextColor: .primaryNormal, valueBackgroundColor: .primaryBg)
             DetailRow(image: "icon_location_16", title: "장소", value: eventDetail.locationInfo.location)
             //DetailRow(image: "icon_calendar", title: "D-Day", value: "D-9", valueTextColor: .red, valueBackgroundColor: .red.opacity(0.2))
@@ -431,8 +431,8 @@ struct DetailRow: View {
             HStack {
                 Image(image)
                 Text(title)
-                    .bodyRegular14()
-                    .foregroundColor(.gray)
+                    .bodyMedium14()
+                    .foregroundColor(.gray100)
             }
             
             Spacer()
