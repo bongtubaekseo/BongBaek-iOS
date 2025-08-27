a162b3c9beff443b38b6d28a41311788d338c35a
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-08-27 18:43:28 +0900

    [fix] LoginView, SignUpBottomView UI 조정

--- BongBaek/BongBaek/Presentation/Login/View/LoginView.swift
+++ BongBaek/BongBaek/Presentation/Login/View/LoginView.swift
@@ -14,27 +14,42 @@ struct LoginView: View {
     @StateObject private var loginViewModel = LoginViewModel()
     
    var body: some View {
-  
+       
        NavigationStack {
-           ZStack {
-               Image("onboarding_ios")
-                   .resizable()
-                   .scaledToFill()
-                   .ignoresSafeArea()
+           VStack(spacing: 0) {
                
                VStack {
                    WelcomeTextView()
                        .padding(.top, 144.adjustedH)
                        .padding(.leading, 20)
+               }
+               
+               Spacer()
+               
+               VStack(spacing: 0) {
                    
-                   Spacer()
-                       .frame(height: 355.adjustedH)
-                   
-                   VStack(spacing: 16) {
-
+                   VStack(spacing: 20) {
                        Button(action: {
                            appStateManager.loginWithKakao()
-                           // ToDo - viewModel.kakaoLogin 시작하는 로직
+                       }) {
+                           Image("appleid_button")
+                               .resizable()
+                               .scaledToFill()
+                               .frame(height: 55.adjustedH)
+
+                       }
+                       .buttonStyle(PlainButtonStyle())
+                       .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+                       .disabled(loginViewModel.isLoading)
+                       .overlay(
+                           loginViewModel.isLoading ?
+                           ProgressView().tint(.white) : nil
+                       )
+                       
+                       Button(action: {
+//                           appStateManager.loginWithKakao()
+                           isPresented.toggle()
+                           
                        }) {
                            Image("btn_kakao")
                                .resizable()
@@ -49,45 +64,46 @@ struct LoginView: View {
                            loginViewModel.isLoading ?
                            ProgressView().tint(.white) : nil
                        )
-                       .padding(.horizontal, 20)
+                   }
+                   .padding(.horizontal,20)
 
-                     
-                       VStack(alignment: .leading,spacing: 8) {
-                           Text("로그인하시면 아래 내용에 동의하는 것으로 간주됩니다.")
+                   VStack(alignment: .leading,spacing: 0) {
+                       Text("로그인하시면 아래 내용에 동의하는 것으로 간주됩니다.")
+                           .captionRegular12()
+                           .foregroundStyle(.white)
+
+                       HStack {
+                           Text("개인정보 처리방침")
                                .captionRegular12()
                                .foregroundStyle(.white)
+                               .underline()
 
-                           HStack {
-                               Text("개인정보 처리방침")
-                                   .captionRegular12()
-                                   .foregroundStyle(.white)
-                                   .underline()
-
-                               Text("이용약관")
-                                   .captionRegular12()
-                                   .foregroundStyle(.white)
-                                   .underline()
-                                   .padding(.leading, 12)
-                           }
-                           .padding(.leading, 50)
-
+                           Text("이용약관")
+                               .captionRegular12()
+                               .foregroundStyle(.white)
+                               .underline()
+                               .padding(.leading, 12)
                        }
-                       .padding(.horizontal, 20)
+                       .padding(.leading, 50)
+                       .padding(.top,12)
                    }
-                   .padding(.bottom, 30)
+                   .padding(.horizontal, 20)
+                   .padding(.top,12.adjustedH)
                }
                
+               Rectangle()
+                   .frame(height: 60.adjustedH)
+                   .foregroundStyle(.clear)
+
            }
-           .navigationDestination(isPresented: $showProfileSetting) {
-               ProfileSettingView()
-           }
-           .alert("로그인 실패", isPresented: $loginViewModel.showError) {
-               Button("확인") { }
-           } message: {
-               Text(loginViewModel.errorMessage ?? "알수 없는 에러가 발생했습니다.")
-           }
+           .background(
+               Image("onboarding_ios")
+                   .resizable()
+                   .scaledToFill()
+                   .ignoresSafeArea()
+           )
        }
-       .sheet(isPresented: $appStateManager.showSignUpSheet) {
+       .sheet(isPresented: $isPresented) {
            SignUpBottomSheetView(
                onComplete: {
                    print("SignUpBottomSheet Clicked()")
@@ -95,7 +111,7 @@ struct LoginView: View {
                    showProfileSetting = true
                }
            )
-           .presentationDetents([.medium])
+           .presentationDetents([.height(439.adjustedH)])
            .presentationDragIndicator(.visible)
        }
    }
