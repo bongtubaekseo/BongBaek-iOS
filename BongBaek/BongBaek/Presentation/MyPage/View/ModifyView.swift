7aa6b9bff0646ce9c70e039060c7f60c1d77aee0
hyunw-kang <vpffp368@naver.com>
2025-08-28 16:55:35 +0900

    [feat] ModifyView 프로필 구현

--- BongBaek/BongBaek/Presentation/MyPage/View/ModifyView.swift
+++ BongBaek/BongBaek/Presentation/MyPage/View/ModifyView.swift
@@ -9,12 +9,26 @@ import SwiftUI
 struct ModifyView: View {
     
     var body: some View {
-        VStack(spacing : 0) {
+        VStack(spacing : 16) {
             Image(.myPageLogo)
                 .frame(width: 110, height: 110)
                 .padding(.top, 9)
                 .padding(.bottom, 9)
+            
+            Text("봉투백서의겸손한야수")
+                .font(.head_bold_24)
+                .foregroundStyle(.gray100)
+            
+            Button(action: {}){
+                Text("내 정보 수정")
+                    .font(.caption_regular_12)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 4)
+                    .background(.primaryNormal)
+                    .cornerRadius(20)
+            }
         }
-        .background(Color.black.ignoresSafeArea())
+        .background(Color.black)
     }
 }
