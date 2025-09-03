f161977bea29a73ce436b3ce66c3d36279b4b9c6
hyunw-kang <vpffp368@naver.com>
2025-09-04 00:03:58 +0900

    [feat] MyPageTarget 설정

---
+++ BongBaek/BongBaek/Network/Base/MyPageTarget.swift
@@ -0,0 +1,70 @@
+//
+//  MyPageTarget.swift
+//  BongBaek
+//
+//  Created by hyunwoo on 9/3/25.
+//
+import Foundation
+import Moya
+
+enum MyPageTarget: TargetType {
+    case getProfile
+    case updateProfile
+}
+
+extension MyPageTarget {
+    var baseURL: URL {
+        guard let url = URL(string: AppConfig.shared.baseURL) else {
+            fatalError("Invalid base URL")
+        }
+        return url
+    }
+    
+    var path: String {
+        switch self {
+        case .getProfile:
+            return "/api/v1/member/profile"
+        case .updateProfile:
+            return "/api/v1/member/profile"
+        }
+    }
+    
+    var method: Moya.Method{
+        switch self{
+        case .getProfile:
+            return .get
+        
+        case .updateProfile:
+            return .put
+        }
+    }
+    
+    var task: Moya.Task{
+        switch self{
+        case .getProfile:
+            return .requestPlain
+        
+        case .updateProfile:
+            return .requestJSONEncodable(profileData)
+        }
+    }
+    
+    var headers: [String : String]?{
+        // 기본 헤더
+        var headers = [
+            "Content-Type": "application/json"
+        ]
+        
+        // KeyChain에서 accessToken 가져오기
+        if let accessToken = KeychainManager.shared.accessToken {
+            headers["Authorization"] = "Bearer \(accessToken)"
+            print("Authorization 헤더 추가됨: Bearer \(accessToken.prefix(10))...")
+        } else {
+            print("AccessToken이 KeyChain에 없습니다")
+        }
+        
+        return headers
+    }
+}
+
+
