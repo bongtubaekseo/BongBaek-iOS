ee06fa4d9bdb7cfcb98d64ddab7932d2c12eed78
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-09-26 23:07:45 +0900

    [refactor] LoginView에 있는 openURL 이동 로직 viewModel 로 분리

---
+++ BongBaek/BongBaek/Presentation/Login/ViewModel/PrivacyURLS.swift
@@ -0,0 +1,28 @@
+//
+//  PrivacyURLS.swift
+//  BongBaek
+//
+//  Created by 임재현 on 9/26/25.
+//
+
+import Foundation
+import UIKit
+
+enum PrivacyUrls: String {
+    // 개인정보처리방침
+    case personalInformationURL = "https://www.notion.so/264f06bb0d3480d0b1eafa217b306105"
+    // 이용약관
+    case termsOfUseURL = "https://www.notion.so/bongtubaekseo/264f06bb0d348036b260f175a236ec7c"
+}
+
+
+
+protocol URLOpenProtocol {
+    func openLink(url: URL)
+}
+
+struct URLOpener: URLOpenProtocol {
+    func openLink(url: URL) {
+        UIApplication.shared.open(url)
+    }
+}
