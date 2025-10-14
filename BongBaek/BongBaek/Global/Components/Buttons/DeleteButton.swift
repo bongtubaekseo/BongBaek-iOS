df30fc2872a7f33c9f02d3120cc3355ff84b92d9
hyunw-kang <vpffp368@naver.com>
2025-10-14 13:11:12 +0900

    [feat] DeleteButton 컴포넌트 구현

---
+++ BongBaek/BongBaek/Global/Components/Buttons/DeleteButton.swift
@@ -0,0 +1,30 @@
+//
+//  DeleteButton.swift
+//  BongBaek
+//
+//  Created by hyunwoo on 10/14/25.
+//
+import SwiftUI
+
+struct DeleteButton: View {
+    let title : String
+    let action : () -> Void
+    
+    var body : some View{
+        Button{
+            action()
+        } label : {
+            Text(title)
+                .font(.title_semibold_18)
+                .foregroundStyle(.secondaryRed)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background()
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.secondaryRed, lineWidth : 1)
+                )
+        }
+    }
+}
