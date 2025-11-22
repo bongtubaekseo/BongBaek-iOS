cfde09a922b0ad8e0aa42332dbb617c2c8a931f7
hyunw-kang <vpffp368@naver.com>
2025-11-22 17:42:15 +0900

    [fix] NewScheduleView Padding값 수정

--- BongBaek/BongBaek/Presentation/Home/Schedule/NewScheduleView.swift
+++ BongBaek/BongBaek/Presentation/Home/Schedule/NewScheduleView.swift
@@ -6,39 +6,37 @@
 //
 import SwiftUI
 
-enum EventCategory : String {
+enum EventCategory: String {
     case wedding = "결혼"
     case birthday = "생일"
     case stoneparty = "돌잔치"
     case funeral = "장례식"
     
     var iconImage: String {
-        switch self{
-        case .wedding :
+        switch self {
+        case .wedding:
             return "icon_alarm"
-        case .birthday :
+        case .birthday:
             return "icon_birthday"
-        case .stoneparty :
+        case .stoneparty:
             return "icon_stoneparty"
-        case .funeral :
+        case .funeral:
             return "icon_funeral"
         }
     }
 }
 
-
-
 struct NewScheduleView: View {
     let event: Event
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 6) {
-            HStack(alignment: .top, spacing: 0) {
+        HStack(alignment: .center, spacing: 0) {  // 최상위를 HStack으로!
+            // 왼쪽: 텍스트 영역
+            VStack(alignment: .leading, spacing: 6) {
                 VStack(alignment: .leading, spacing: 4) {
                     Text("경조사 알림")
                         .font(.caption_regular_12)
                         .foregroundColor(.txtDisplaySubtle)
-                        .padding(.top, 6)
 
                     if event.eventInfo.dDay == 0 {
                         Text("오늘은 \(event.hostInfo.hostName)님의 \(event.eventInfo.eventCategory)입니다!")
@@ -53,39 +51,42 @@ struct NewScheduleView: View {
                     }
                 }
 
-                Spacer()
+                HStack(spacing: 4) {
+                    Image(.iconCalendar)
+                        .resizable()
+                        .frame(width: 14, height: 14)
 
-                Image(getCategoryIcon())
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 94, height: 82)
+                    Text(event.eventInfo.eventDate.DateFormat())
+                        .font(.caption_regular_12)
+                        .foregroundColor(.gray100)
+                }
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .background(.bgDisplayPrimary)
+                .cornerRadius(2)
             }
-
-            HStack(spacing: 4) {
-                Image(.iconCalendar)
-                    .resizable()
-                    .frame(width: 14, height: 14)
-
-                Text(event.eventInfo.eventDate.DateFormat())
-                    .font(.caption_regular_12)
-                    .foregroundColor(.gray100)
-            }
-            .padding(.horizontal, 8)
-            .padding(.vertical, 4)
-            .background(.bgDisplayPrimary)
-            .cornerRadius(2)
+            .padding(.leading, 16)
+            
+            Spacer()
+            
+            // 오른쪽: 이미지 (자동으로 세로 중앙!)
+            Image(getCategoryIcon())
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 94, height: 82)
+                .padding(.trailing, 12)
         }
-        .padding(.horizontal, 16)
         .padding(.vertical, 10)
-        .frame(maxWidth: .infinity, alignment: .leading)
         .background(.bgDisplaySecondary)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
         .overlay(
             RoundedRectangle(cornerRadius: 8)
                 .stroke(Color.borderFieldDefault, lineWidth: 1)
         )
         .padding(.horizontal, 20)
     }
+    
     private func getCategoryIcon() -> String {
-            EventCategory(rawValue: event.eventInfo.eventCategory)?.iconImage ?? "icon_alarm"
-        }
+        EventCategory(rawValue: event.eventInfo.eventCategory)?.iconImage ?? "icon_alarm"
+    }
 }
