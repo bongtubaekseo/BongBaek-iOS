8d8387fffe2e112c3e9c6e9e6d23b2ec3a92f4b2
hyunw-kang <vpffp368@naver.com>
2025-11-24 15:48:26 +0900

    [fix] ContentsCardView Button 적용범위 변경

--- BongBaek/BongBaek/Presentation/Home/Contents/ContentsCardView.swift
+++ BongBaek/BongBaek/Presentation/Home/Contents/ContentsCardView.swift
@@ -14,26 +14,26 @@ struct ContentsCardView: View {
     let title: String
     
     var body: some View {
-        ZStack(alignment: .topLeading){
-            RoundedRectangle(cornerRadius: 6)
-                .fill(Color.btnInteractiveSecondary)
-                .frame(width: 220, height: 256)
-            
-            VStack(alignment: .leading, spacing: 0) {
-                Image(image)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 220, height: 160)
-                    .clipped()
+        Button(action : {
+            router.push(to : .contentDetailView)
+        }){
+            ZStack(alignment: .topLeading){
+                RoundedRectangle(cornerRadius: 6)
+                    .fill(Color.btnInteractiveSecondary)
+                    .frame(width: 220, height: 256)
                 
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(category)
-                        .font(.caption_regular_12)
-                        .foregroundStyle(.txtStatusFocused)
+                VStack(alignment: .leading, spacing: 0) {
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 220, height: 160)
+                        .clipped()
                     
-                    Button(action : {
-                        router.push(to : .contentDetailView)
-                    }){
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(category)
+                            .font(.caption_regular_12)
+                            .foregroundStyle(.txtStatusFocused)
+                        
                         HStack {
                             Text(title)
                                 .font(.body1_medium_16)
@@ -46,14 +46,14 @@ struct ContentsCardView: View {
                                 .frame(width: 20, height: 20)
                         }
                     }
+                    .padding(12)
+                    .frame(maxWidth: .infinity, alignment: .leading)
                 }
-                .padding(12)
-                .frame(maxWidth: .infinity, alignment: .leading)
             }
+            .buttonStyle(PlainButtonStyle())
+            .background(Color.btnInteractiveSecondary)
+            .frame(width: 220, height: 256)
+            .clipShape(RoundedRectangle(cornerRadius: 6))
         }
-        .buttonStyle(PlainButtonStyle())
-        .background(Color.btnInteractiveSecondary)
-        .frame(width: 220, height: 256)
-        .clipShape(RoundedRectangle(cornerRadius: 6))
     }
 }
