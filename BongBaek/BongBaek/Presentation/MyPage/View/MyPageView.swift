1845f07456753d937e9701b5ff2440ca0560724d
hyunw-kang <vpffp368@naver.com>
2025-12-14 21:55:40 +0900

    [fix] #157 MypageView arrow버튼 삭제

--- BongBaek/BongBaek/Presentation/MyPage/View/MyPageView.swift
+++ BongBaek/BongBaek/Presentation/MyPage/View/MyPageView.swift
@@ -58,14 +58,9 @@ struct MyPageView: View {
             //Color.black.ignoresSafeArea()
             VStack(spacing: 0) {
                 HStack {
-//                    Button(action: {
-//                        router.pop()
-//                    }) {
-//                        Image(systemName: "chevron.left")
-//                            .foregroundColor(.gray300)
-//                    }
-//                    .frame(width: 44, height: 44)
-//                    
+                    Color.clear
+                    .frame(width: 44, height: 44)
+//
                     Spacer()
                     
                     Text("설정")
